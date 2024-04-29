58c685b27d1b2fb4d61679a36b3aa0ea24adfe34
LaraGomezN <111064261+LaraGomezN@users.noreply.github.com>
2024-04-29 14:54:24 -0500

    Taller funcionando

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SerieModule } from './serie/serie.module';
-
-
 
 @NgModule({
   declarations: [
@@ -14,7 +13,8 @@ import { SerieModule } from './serie/serie.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    SerieModule
+    SerieModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/serie/serie.component.ts
+++ src/app/serie/serie.component.ts
@@ -1,22 +1,41 @@
 import { Component, OnInit } from '@angular/core';
 import {Serie} from './serie';
-import {dataSeries} from './dataSeries';
+import {SerieService} from './serie.service';
 
 @Component({
   selector: 'app-serie',
   templateUrl: './serie.component.html',
   styleUrls: ['./serie.component.css']
 })
+
 export class SerieComponent implements OnInit {
 
   series: Array<Serie> = [];
-  constructor() { }
-  getSerieList(): Array<Serie> {
-    return dataSeries;
+  promedio: number = 0;
+
+  constructor(private serieService: SerieService) { }
+
+  getSeries() {
+    this.serieService.getSeries().subscribe(series =>
+      {this.series=series;
+        this.getPromedio();
+      }
+    )
+  }
+
+  getPromedio(){
+    let seasons: number = 0;
+    let total: number = 0;
+    for (let i=0; i<this.series.length; i++){
+      seasons += this.series[i].seasons;
+      total += 1;
+    }
+    this.promedio = Math.round(seasons/total);
+
   }
 
   ngOnInit() {
-    this.series = this.getSerieList();
+    this.getSeries();
   }
 
 }
